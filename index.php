590f824127a6b0627880a9912bb0d58f49f2ff9b
Florian Leuchtenstern <Florian@Leuchtenstern.com>
2020-04-29 00:34:29 +0200

    Initialer Commit

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+
+<html>
+
+    <head>
+    
+    <title>Leuchtenstern</title>
+    
+        <meta name="viewport"
+        	content="width=device-width, initial scale=1.0, maximum scale=1.0" 
+        	charset="UTF-8" />
+        
+        <link rel="stylesheet"
+        	href="<?php echo 'styles/styles.css?v=' . filemtime('styles/styles.css'); ?>">
+    
+    </head>
+
+	<body class="distribution" oncontextmenu="return false">
+	   
+	   <!-- Unterdrückt Rechtsklick -->
+
+		<p>Florian@Leuchtenstern.com</p>
+
+	</body>
+
+
+</html>
